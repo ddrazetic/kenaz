01c66f0fe5ae7bfcaf270ddac035f442d3c52927
Danijel Drazetic <daddo.1009@gmail.com>
2021-11-09 18:30:28 +0100

    header done

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './components/Header';
+import Homepage from './components/Homepage';
+
+
+const App = () =>{
+  return (
+    <div className="App">
+       <Header/>
+    </div>
+  );
+}
+
+export default App;
